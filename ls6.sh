0a91fa2022c487d951b21373b1fd4db4a08be942
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-06 08:31:26 -0600

    adding gcc13

--- ls6.sh
+++ ls6.sh
@@ -10,7 +10,7 @@ for s in *.spec ; do
 done
 
 cat install.sh \
-| sed -e 's/frontera/ls6/' -e 's/COMPILERS/i240,j21_11 i19,j19_0 g112,j19_0/' \
+| sed -e 's/frontera/ls6/' -e 's/COMPILERS/i240,j21_11 i19,j19_0 g112,j19_0 g132,j19_0/' \
 > ls6_specfiles/install.sh
 
 cd ls6_specfiles

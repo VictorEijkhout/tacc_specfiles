2ea48eea0446266612dae6518056adb1f7a896cf
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-09-21 14:51:01 -0500

    ls6 i26

--- ls6.sh
+++ ls6.sh
@@ -21,7 +21,7 @@ for s in *.spec ; do
 done
 
 cat install.sh \
-    | sed -e 's/frontera/ls6/' -e 's/COMPILERS/i19,j19_0 i241,j21_12 i253,j21_17 g112,j19_0 g132,j21_12/' \
+    | sed -e 's/frontera/ls6/' -e 's/COMPILERS/i19,j19_0 i241,j21_12 i253,j21_17 i261,j21_18 g112,j19_0 g132,j21_12/' \
     > ls6_specfiles/install.sh
 
 cd ls6_specfiles
